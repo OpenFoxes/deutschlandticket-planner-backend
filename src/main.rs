3d701b26a34c5ea7763c34f591fd831842f88c0c
Philipp Melzer <philipp.melzer2014+git@gmail.com>
2025-03-16 00:27:17 +0100

    ✨ Hello world in Axum
    Signed-off-by: Philipp Melzer <philipp.melzer2014+git@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::Router;
+    use axum_test::TestServer;
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = Router::new().route("/", get(handler));
+        let client = TestServer::new(app);
+        let response = client.expect("").get("/").await;
+
+        assert_eq!(response.status_code(), 200);
+        assert_eq!(response.text(), "Hello, world!");
+    }
 }
