403485dd92d1f8701ad0d608cda915eb564779c9
Bono Fox <git@bono-fox.de>
2025-03-22 20:08:16 +0100

    💄 Styling für die Dokumentation (#15)
    - Dokumentation im Darkmode gestyled
    
    https://github.com/darshandsoni/asciidoctor-skins/blob/gh-pages/css/dark.css

--- doc/index.js
+++ doc/index.js
@@ -12,33 +12,38 @@ const docsDir = "generated";
 console.log(`${documents.length} Dateien zu erzeugen`);
 
 for (const document of documents) {
-    const fileInfo = extractFileParts(document);
-    console.log(`Erzeuge ${fileInfo.filename} unter ${fileInfo.dir}`);
-    // Asciidoctor.convertFile(document);
-    Asciidoctor.convertFile(document, {
-        safe: "safe",
-        attributes: { linkcss: true },
-        mkdirs: true,
-        to_dir: `${docsDir}/${fileInfo.dir}`,
-    });
+  const fileInfo = extractFileParts(document);
+  console.log(`Erzeuge ${fileInfo.filename} unter ${fileInfo.dir}`);
+  Asciidoctor.convertFile(document, {
+    safe: "safe",
+    attributes: {
+      linkcss: true,
+      stylesheet: fileInfo.pathToDirRoot + "dark.css",
+    },
+    mkdirs: true,
+    to_dir: `${docsDir}/${fileInfo.dir}`,
+  });
 }
 
 function extractFileParts(filepath) {
-    filepath = filepath.replaceAll("\\", "/");
+  filepath = filepath.replaceAll("\\", "/");
 
-    if (!filepath.includes("/")) {
-        return { filename: filepath, dir: "" };
-    }
+  if (!filepath.includes("/")) {
+    return { filename: filepath, dir: "", pathToDirRoot: "./" };
+  }
 
-    if (filepath.endsWith("/")) {
-        filepath = filepath.substring(0, filepath.length - 1);
-    }
+  if (filepath.endsWith("/")) {
+    filepath = filepath.substring(0, filepath.length - 1);
+  }
 
-    return {
-        filename: filepath.substring(
-            filepath.lastIndexOf("/") + 1,
-            filepath.length,
-        ),
-        dir: filepath.substring(0, filepath.lastIndexOf("/")),
-    };
+  const directoryDepth = filepath.split('"/').length - 1;
+
+  return {
+    filename: filepath.substring(
+      filepath.lastIndexOf("/") + 1,
+      filepath.length,
+    ),
+    dir: filepath.substring(0, filepath.lastIndexOf("/")),
+    pathToDirRoot: "../".repeat(directoryDepth),
+  };
 }
