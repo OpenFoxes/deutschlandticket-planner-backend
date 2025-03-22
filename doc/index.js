66f148f276d6d67aa1e71dfdeb0243d0d1139429
Bono Fox <git@bono-fox.de>
2025-03-22 18:55:28 +0100

    ✨ Dokumentation kann generiert werden (#15)
    - Script generiert alle darunterliegenden Asciidoc-Dateien
     als HTML-Dateien

---
+++ doc/index.js
@@ -0,0 +1,44 @@
+const asciidoctor = require("asciidoctor");
+const { Glob } = require("glob");
+const { copyFile, mkdirSync } = require("fs");
+
+const Asciidoctor = asciidoctor();
+
+console.log("Generiere Dokumentation ...", "...", "...");
+
+const documents = new Glob("src/**/*.adoc", { nocase: true }).walkSync();
+const docsDir = "generated";
+
+console.log(`${documents.length} Dateien zu erzeugen`);
+
+for (const document of documents) {
+    const fileInfo = extractFileParts(document);
+    console.log(`Erzeuge ${fileInfo.filename} unter ${fileInfo.dir}`);
+    // Asciidoctor.convertFile(document);
+    Asciidoctor.convertFile(document, {
+        safe: "safe",
+        attributes: { linkcss: true },
+        mkdirs: true,
+        to_dir: `${docsDir}/${fileInfo.dir}`,
+    });
+}
+
+function extractFileParts(filepath) {
+    filepath = filepath.replaceAll("\\", "/");
+
+    if (!filepath.includes("/")) {
+        return { filename: filepath, dir: "" };
+    }
+
+    if (filepath.endsWith("/")) {
+        filepath = filepath.substring(0, filepath.length - 1);
+    }
+
+    return {
+        filename: filepath.substring(
+            filepath.lastIndexOf("/") + 1,
+            filepath.length,
+        ),
+        dir: filepath.substring(0, filepath.lastIndexOf("/")),
+    };
+}
